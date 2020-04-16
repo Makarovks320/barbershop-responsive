6d21fa7686a3277c4fcd243aeeb2c914e8974163
Makarovks320 <kirill.makarov@tafs.pro>
2020-04-16 03:48:43 +0300

    перенастроил галп: минифицировал картинки, css, js, настроил npm run build и start

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,96 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var plumber= require('gulp-plumber');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');  /*запирает все ошибки*/
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const uglifyEs = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+
+// const uglify = require('gulp-uglify');
+
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/style.scss')
-    .pipe(plumber())
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
+function styles() {
+    return gulp.src('./sass/style.scss')
+                .pipe(plumber())
+                .pipe(sass({
+                    includePaths: require('node-normalize-scss').includePaths
+                }).on('error', sass.logError))
+                .pipe(autoprefixer({
+                    /*browsers: ['last 2 versions'], - убрал в package.json в "browserslist"*/
+                    cascade: false
+                }))
+                .pipe(gulp.dest('./build/css'))
+                .pipe(cleanCSS({level: 2}))
+                .pipe(rename('syle-min.css'))
+                .pipe(gulp.dest('./build/css'))
+                .pipe(browserSync.stream()); /*Команда перезагрузки сервера в браузере*/
+}
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', gulp.parallel('sass'));
-});
+
+const jsFiles = ['./js/menu.js'];
+function scripts() {
+    return gulp.src(jsFiles)
+                .pipe(concat('scripts.js'))
+                .pipe(gulp.dest('./build/js'))
+                .pipe(rename('scripts-min.js'))
+                .pipe(uglifyEs({
+                    toplevel: true
+                }))
+                .pipe(gulp.dest('./build/js'))
+                .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({ /*запустит локальный сервак на node.js*/
+        server: {baseDir: "./"} /*указывает, где искать html-файлы*/
+    });
+    gulp.watch('./sass/**/*.scss', styles);
+    gulp.watch('./js/**/*.js', scripts);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+}
+
+function clean() {
+    return del(['./build/*']);  /*требуется return, иначе ошибка: The following tasks did not complete: clean*/
+}
+
+function copy() {
+    return gulp.src(['./fonts/**/*.{woff, woff2}', './img/**'],
+                     {base: "./"})
+                .pipe(gulp.dest('./build'))
+}
+
+function sprite() {
+    return
+}
+// добавить webp, svgstore
+function images() {
+    return gulp.src('./build/img/*.{png,jpg,gif}') /*берем любой .png, .jpg, .gif файл в любой подпапке папки img*/
+      .pipe(imagemin([      /*imagemin сам по себе содержит в себе множество плагинов (работа с png,svg,jpg и тд)*/
+        imagemin.optipng({optimizationLevel: 3}), /* 1 - максимальное сжатие, 3 - безопасное сжатие, 10 - без сжатия*/
+        imagemin.mozjpeg({progressive: true}),   /*прогрессивная загрузка jpg (сначала пиксельная, позже проявляется)*/
+        ]))
+      .pipe(gulp.dest('./build/img'));
+  };
+
+gulp.task('clean', clean);
+gulp.task('watch', watch);
+gulp.task('build', gulp.series(clean, /*gulp.series синхронно и последовательно запускает функции*/
+                                gulp.parallel(styles, scripts, copy), /*асинхронно*/
+                                images
+                                ));
+
+
+
+
+
+
+
+
