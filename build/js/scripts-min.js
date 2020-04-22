1b79fb27cc3742c180bffd0a63439ac3c61f03e3
Makarovks320 <kirill.makarov@tafs.pro>
2020-04-22 22:00:08 +0300

    инициализировал build

---
+++ build/js/scripts-min.js
@@ -0,0 +1,1 @@
+const n=document.querySelector(".main-nav");document.querySelector(".main-nav__toggle").addEventListener("click",(function(){n.classList.contains("main-nav--closed")?(n.classList.remove("main-nav--closed"),n.classList.add("main-nav--opened")):(n.classList.add("main-nav--closed"),n.classList.remove("main-nav--opened"))}));
